2e386bdd5aa68bde67a428d69cc947ddeb4658d4
Philip Schleihauf <uniphil@gmail.com>
2012-08-08 19:24:11 -0400

    convert tabs to spaces

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='SuperHash',
-      version='0.1.1',
-      author='Philip Schleihauf',
-      author_email='uniphil@gmail.com',
-      license='GPLv3',
-      description='Hash anything',
-      long_description=open('README.md').read(),
-      py_modules=['superhash'],
-      url='https://github.com/uniphil/SuperHash',
-      )
+setup(
+	name='SuperHash',
+    version='0.1.2',
+    author='Philip Schleihauf',
+    author_email='uniphil@gmail.com',
+    license='GPLv3',
+    description='Hash anything',
+    long_description=open('README.md').read(),
+    py_modules=['superhash'],
+    url='https://github.com/uniphil/SuperHash',
+    )
