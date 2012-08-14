b31f5b178a21009b62af82fb72acfde5ae73b445
Philip Schleihauf <uniphil@gmail.com>
2012-08-14 17:00:23 -0400

    Add hashable wrapper

--- superhash.py
+++ superhash.py
@@ -1,6 +1,52 @@
-"""hash anything"""
+"""
+hash anything.
 
-def superhash(anything):
+
+>>> from superhash import superhash
+>>> superhash({'look':'I','m':'hashing','a':'dictionary!'})
+6096922376629631909
+>>> superhash(['nested', ['data', ['structure?', ['no', ['problem!']]]]])
+-1209686700893922918
+>>> class Foo(object): pass
+... 
+>>> foo = Foo()
+>>> superhash(foo)
+8783925544637
+>>> foo.a = 1
+>>> superhash(foo)
+-3828842421826240771
+
+
+>>> from superhash import hashable
+>>> @hashable
+... class HashWrapped(dict): pass
+... 
+>>> hash(HashDecorated)
+1760438
+>>> hash(HashDecorated({1:2}))
+3713081631934410656
+
+
+>>> from superhash import hashable
+>>> class HashableDict(hashable(dict)): pass
+... 
+>>> hash(HashableDict)
+2533830
+>>> hd = HashableDict({1:2})
+>>> hash(hd)
+3713081631934410656
+>>> hd.update({1:3})
+>>> hash(hd)
+3713081631933328131
+
+
+>>> from superhash import hashable
+>>> inline = hashable(dict)({1:2})
+>>> hash(inline)
+3713081631934410656
+"""
+
+def superhash(anything, try_anyway=True):
     """takes anything. returns a hash of the anything."""
     
     xhash = 0
@@ -12,18 +58,40 @@ def superhash(anything):
         pass
 
     try:
-        # can python hash it already?
-        xhash ^= hash(anything)
+        if try_anyway:
+            # can python hash it already?
+            xhash ^= hash(anything)
+        else:
+            raise TypeError('can\'t hash that')
     except TypeError:
         try:
             # is it dict-like?
             for item in anything.items():
                 xhash ^= superhash(item)
         except AttributeError:
-            # is it iterable?
-            for item in anything:
-                xhash ^= superhash(item)
+            try:
+                # is it iterable?
+                for item in anything:
+                    xhash ^= superhash(item)
+            except TypeError:
+                pass
     
     return hash(xhash)
 
 
+def hashable(thing):
+    """make anything hashable"""
+
+    # functions are already hashable
+    if type(thing) is type(lambda: 0):
+        return thing
+
+    # classes, on the other hand...
+    else:
+        class HashableClass(thing):
+            __doc__ = '%s\n\nThis thing is hashable.' % thing.__doc__
+            __class__ = thing().__class__
+            def __hash__(self):
+                return superhash(self, try_anyway=False)
+
+        return HashableClass
--- test/test_superhash.py
+++ test/test_superhash.py
@@ -2,7 +2,7 @@
 import unittest
 from .. import superhash as sh
 
-class TestVarsToFile(unittest.TestCase):
+class TestHasher(unittest.TestCase):
 
 	def assertHash(self, in1, in2):
 		h1 = sh.superhash(in1)
@@ -51,6 +51,11 @@ class TestVarsToFile(unittest.TestCase):
 		class cls2(object): y=1
 		self.assertHash(cls1, cls2)
 
+	def testIdentiClass(self):
+		class cls1(object): x=1
+		class cls2(object): x=1
+		self.assertEqual(sh.superhash(cls1), sh.superhash(cls2))
+
 	def testObject(self):
 		class cls(object): pass
 		inst1, inst2 = cls(), cls()
@@ -91,6 +96,71 @@ class TestVarsToFile(unittest.TestCase):
 		self.assertHash(ob1, ob2)
 
 
+class TestHashableClassable(unittest.TestCase):
+
+	def testHashNonHashableClasses(self):
+		class HashDict(sh.hashable(dict)): pass
+		hash(HashDict)
+		class HashList(sh.hashable(list)): pass
+		hash(HashList)
+
+	def testHashNonHashableInstance(self):
+		class HashDict(sh.hashable(dict)): pass
+		h1 = hash(HashDict({1:1}))
+
+	def testInstanceHashesValid(self):
+		class HashDict(sh.hashable(dict)): pass
+		d1, d2 = HashDict(), HashDict()
+		self.assertEqual(hash(d1), hash(d2))
+
+	def testInstanceHashesNEQ(self):
+		class HashDict(sh.hashable(dict)): pass
+		d1, d2 = HashDict(), HashDict()
+		d1.x = 1
+		self.assertNotEqual(hash(d1), hash(d2))
+
+	def testDocPassthrough(self):
+		non_hashable_doc = dict.__doc__
+		hashable_doc = sh.hashable(dict).__doc__
+		self.assertIn(non_hashable_doc, hashable_doc)
+
+	def testClassPassthrough(self):
+		dict_class = dict().__class__
+		hashdict_class = sh.hashable(dict)().__class__
+		self.assertIs(dict_class, hashdict_class)
+
+	def testLambda(self):
+		l1 = sh.hashable(lambda:0)
+		hash(l1)
+		l2 = sh.hashable(lambda:1)
+		self.assertNotEqual(hash(l1), hash(l2))
+
+	def testFunctionWrap(self):
+		@sh.hashable
+		def f(x):
+			return x
+		hash(f)
+		@sh.hashable
+		def f2(x):
+			return x*2
+		self.assertNotEqual(hash(f), hash(f2))
+
+	def testClassWrap(self):
+		@sh.hashable
+		class C(object): pass
+		hash(C)
+		hash(C())
+		class C2(object): pass
+		self.assertEqual(hash(C), hash(C2))
+		self.assertEqual(hash(C()), hash(C2()))
+		C2.x = 4
+		self.assertNotEqual(hash(C), hash(C2))
+		self.assertNotEqual(hash(C()), hash(C2()))
+		c = C2()
+		hash(c)
+		c2 = C2()
+		c2.y = 6
+		self.assertNotEqual(hash(c), hash(c2))
 
 
 if __name__ == '__main__':
